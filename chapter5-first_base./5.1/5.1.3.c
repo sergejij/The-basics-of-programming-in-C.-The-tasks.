9a76db803393d3382aef1c06f0c202dd0dab9911
sergejij <ushakoff.sergej2010@yandex.ru>
2019-02-02 18:27:26 +0400

    5.1.3

--- chapter5-first_base./5.1/5.1.3.c
+++ chapter5-first_base./5.1/5.1.3.c
@@ -4,11 +4,14 @@
 // Идёт k секунда суток. Определить, сколько целых часов и целых минут прошло с начала суток. 
 
 int main() {
-  int seconds, minutes, hours;
-  scanf("%d", &seconds);
-
-  hours = seconds/3600;
-  minutes =  (int)seconds % 60;
-  printf("%d %d\n", hours, minutes);
+  // put your code here
+  int k, h, m, allMinuts;
+  scanf("%d", &k);
+  allMinuts = k / 60;
+  h = allMinuts / 60;
+  m = allMinuts % 60;
+    
+  printf("%d %d\n", h, m);
+  
   return 0;
 }
